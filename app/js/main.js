3746776586223515662fb2711bf7e61ff262425d
XkaratistX <stomatolog456@gmail.com>
2021-03-27 22:15:43 +0100

    added filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
+
+
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onChange: function(data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+    });
+
+
     $('.slider__inner').slick({
         arrows: false,
         dots: true,
