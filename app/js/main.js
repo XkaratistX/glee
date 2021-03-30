001d19cc65ad9ad86eab58b9ab3809478c4b28e8
XkaratistX <stomatolog456@gmail.com>
2021-03-30 19:46:32 +0200

    added details content

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
+    $('.product-details__num').styler();
+
+    $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false
+    });
+    $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade: true
+    });
+
     $(".filter-recent__star").rateYo({
         starWidth: "17px",
         ratedFill: "#ffcc00",
@@ -8,6 +25,13 @@ $(function() {
     });
 
     $(".shop-content__star").rateYo({
+        starWidth: "17px",
+        ratedFill: "#ffcc00",
+        normalFill: "#d6d6d6",
+        readOnly: true
+    });
+
+    $(".product-details__star").rateYo({
         starWidth: "17px",
         ratedFill: "#ffcc00",
         normalFill: "#d6d6d6",
